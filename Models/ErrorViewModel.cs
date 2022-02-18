929a7192f686ce9ff245c3d058ca7e75a4de01c5
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-18 16:28:45 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PARTNER.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
