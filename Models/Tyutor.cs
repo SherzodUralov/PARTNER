0aa446a01b09f9a98e38d5f3f9fb2a062922812b
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-18 23:46:41 +0500

    klass qo'shildi

---
+++ Models/Tyutor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARTNER.Models
+{
+    public class Tyutor
+    {
+        public int TyutorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SureName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Staj { get; set; }
+        public string MobileNamber { get; set; }
+        public string TelegramNamber { get; set; }
+        public int Image { get; set; }
+        public string Resime { get; set; }
+        public int Gender { get; set; }
+        public int Degree { get; set; }
+
+    }
+}
