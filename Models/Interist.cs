3a47608c4206a85e32bf8466cc503e52875e9d87
jahongirsuyunov836@gmail.com <jahongirsuyunov836@gmail.com>
2022-02-18 22:34:14 +0300

    Add Arts,Musics, Sports, Scientifics enum

--- Models/Interist.cs
+++ Models/Interist.cs
@@ -8,10 +8,10 @@ namespace PARTNER.Models
     public class Interist
     {
         public int MyProperty { get; set; }
-        public int Music { get; set; }
-        public int Art { get; set; }
-        public int Sport { get; set; }
-        public int Scientific { get; set; }
+        public Musics? Music { get; set; }
+        public Arts? Art { get; set; }
+        public Sports? Sport { get; set; }
+        public Scientifics? Scientific { get; set; }
         public string BoshqaSoxa { get; set; }
     }
 }
