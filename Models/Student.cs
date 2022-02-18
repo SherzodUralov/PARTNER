e160740fa392af9bed5b521d85ca71cbd1beb952
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-18 22:43:33 +0500

    kllas yaratildi

---
+++ Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARTNER.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SureName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Passport { get; set; }
+        public int Gender { get; set; }
+        public string MobileNamber { get; set; }
+        public string TelegramNamber { get; set; }
+        public string Nation { get; set; }
+        public string ParentsId { get; set; }
+        public string InteristId { get; set; }
+        public int DistrictId { get; set; }
+        public string Mahalla { get; set; }
+        public int GroupId { get; set; }
+        public int CoursId { get; set; }
+        public int LiveId { get; set; }
+        public int Contract { get; set; }
+        public int Familly { get; set; }
+        public int KamTam { get; set; }
+        public string BoquvchiId { get; set; }
+        public int NogironID { get; set; }
+        public int DegreeId { get; set; }
+
+
+
+    }
+}
