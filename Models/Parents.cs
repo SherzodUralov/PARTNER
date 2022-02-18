d1b8715e168eb1b26050f329777082e8371e6d9f
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-18 23:02:25 +0500

    o'zgarish

---
+++ Models/Parents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARTNER.Models
+{
+    public class Parents
+    {
+        public int ParentsId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SureName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string MobileNamber { get; set; }
+        public string TelegramNamber { get; set; }
+        public int DistrictId { get; set; }
+        public int NogironId { get; set; }
+        public string Mahalla { get; set; }
+        public int Jobs { get; set; }
+
+
+    }
+}
