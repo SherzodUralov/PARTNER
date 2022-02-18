f18b3e4b36b2b536d769153e38dc44aad19b6d65
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-19 00:13:30 +0500

    interist kllasi qo'shildi

---
+++ Models/Interist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARTNER.Models
+{
+    public class Interist
+    {
+        public int MyProperty { get; set; }
+        public int Music { get; set; }
+        public int Art { get; set; }
+        public int Sport { get; set; }
+        public int Scientific { get; set; }
+        public string BoshqaSoxa { get; set; }
+    }
+}
