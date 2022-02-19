bc0807aba6dc660b12398ab5be4ff711d1696a1f
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-20 01:09:09 +0500

    Adedd classs new

---
+++ Models/Speciality.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARTNER.Models
+{
+    public class Speciality
+    {
+        public int SpecialityId { get; set; }
+        public int SpecialityName { get; set; }
+        public string Years { get; set; }
+        public int TyutorId { get; set; }
+    }
+}
