993bcf724e8e6fa0646ebb8f7b66861e5e12aa40
jahongirsuyunov836@gmail.com <jahongirsuyunov836@gmail.com>
2022-02-21 20:44:31 +0300

    Check update

--- Models/Check.cs
+++ Models/Check.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,23 @@ namespace PARTNER.Models
 {
     public class Check
     {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("CheckId")]
+        [Display(Name = "CheckId")]
         public int CheckId { get; set; }
+        [Required]
+        [Column("YearMonth")]
+        [Display(Name = "YearMonth")]
         public string YearMonth { get; set; }
+        [Required]
+        [Column("Timee")]
+        [Display(Name = "Timee")]
         public int Timee { get; set; }
-        public Uzlashtrish Uzlashtirish { get; set; }
+        [Required]
+        [Column("Uzlashtirish")]
+        [Display(Name = "Uzlashtirish")]
+        public Uzlashtrish? Uzlashtirish { get; set; }
     }
 }
