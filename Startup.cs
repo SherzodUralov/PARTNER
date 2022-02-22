55e4504cf82560f15abc5d0bd8b0507c7b23a7da
jahongirsuyunov836@gmail.com <jahongirsuyunov836@gmail.com>
2022-02-22 20:26:53 +0300

    conniction

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PARTNER.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,7 @@ namespace PARTNER
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContextPool<AppDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("PARTNER_DB"))
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
