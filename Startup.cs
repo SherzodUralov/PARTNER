f1612d50ef61583f0cea77ad84638f2432813c0c
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-22 22:10:15 +0500

    update

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PARTNER.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,17 +15,20 @@ namespace PARTNER
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            //services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("PARTNER_DB")));
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +44,10 @@ namespace PARTNER
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+          
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+           // app.UseMvcWithDefaultRoute();
 
             app.UseRouting();
 
